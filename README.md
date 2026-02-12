53683357ba6c0707dabd599ff23019dba7c81d0b
yuiyuiui <2946723935@qq.com>
2026-02-13 05:42:19 +0800

    format

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## 1. Basic
+# 1. Basic
 We have a input signal on a uniform grid of $[L_0=N_0h, L=Nh]$. The signal is 
 $$f(x) = c_1x^{-a} + c_2x^{-a-d}+O(x^{-a-d-d_0})$$
 
@@ -11,15 +11,15 @@ $$f(x) = \sum_{k=1}^n c_k x^{-a_k} + O(x^{-a_n-d_0})$$
 
 $$a_n>...>a_1>0, ~a_{k+1}-a_k\geq d_0>0, c_k\in\mathbb{R}$$
 
-## 2. Method
+# 2. Method
 
 1. Common Prony
 2. Find leading order with high accuracy and then find other orders
 3. Direct LSQ to find orders with dichotomy
 
-## 3. Result
+# 3. Result
 
-#### 1. Leading Item
+### 1. Leading Item
 
 
 $$f(x) = c_1x^{-a_1} + c_2x^{-a_2} + c_3x^{-a_3} * sin(x^2)$$
--- test/leading.jl
+++ test/leading.jl
@@ -1,33 +1,43 @@
 @testset "leading.jl" begin
-    T = Float64
-    a1 = T(0.55)
-    a2 = T(1.22)
-    a3 = T(1.83)
-    c1, c2, c3 = rand(T, 3) .+ 1
+    Random.seed!(123456789)
+    a1 = 0.55
+    a2 = 1.22
+    a3 = 1.83
+    c1, c2, c3 = rand(3) .+ 1
     #=
-     1.0302671169218902
+    1.0302671169218902
     1.0857778139437397
     1.114727702313528
-
     =#
-    f = x -> c1*x^(-a1) + c2*x^(-a2) + c3 * x^(-a3) * sin(x^2)
-    L0 = 1
-    L = 2^14
-    point_density = 10
-    h = T(1/point_density)
-    grid = [L0 + i * h for i in 0:((L - L0) * point_density)]
-    f_data = f.(grid)
+    for T in [Float32, Float64, BigFloat]
+        @show T
+        _a1, _a2, _a3 = T.([a1, a2, a3])
+        _c1, _c2, _c3 = T.([c1, c2, c3])
+        f = x -> _c1 * x^(-_a1) + _c2 * x^(-_a2) + _c3 * x^(-_a3) * sin(x^2)
+        L0 = 1
+        L = 2^14
+        point_density = 10
+        h = T(1 / point_density)
+        grid = [L0 + i * h for i in 0:((L - L0) * point_density)]
+        f_data = f.(grid)
 
-    order_loglog, coff_loglog, _ = loglog_fit(f_data, grid)
-    order_shanks, coff_shanks = shanks_int_fit(f_data, grid)
+        order_loglog, coff_loglog, _ = loglog_fit(f_data, grid)
+        order_shanks, coff_shanks = shanks_int_fit(f_data, grid)
 
-    @show abs(order_loglog - a1)
-    @show abs(coff_loglog - c1)
-    @show abs(order_shanks - a1)
-    @show abs(coff_shanks - c1)
+        @show abs(order_loglog - _a1)
+        @show abs(coff_loglog - _c1)
+        @show abs(order_shanks - _a1)
+        @show abs(coff_shanks - _c1)
 
-    @test abs(order_loglog - a1) < 0.0072
-    @test abs(coff_loglog - c1) < 0.071
-    @test abs(order_shanks - a1) < 1.4e-5
-    @test abs(coff_shanks - c1) < 7e-4
+        @test order_loglog isa T
+        @test coff_loglog isa T
+        @test order_shanks isa T
+        @test coff_shanks isa T
+        if T == Float64
+            @test abs(order_loglog - _a1) < 0.0072
+            @test abs(coff_loglog - _c1) < 0.071
+            @test abs(order_shanks - _a1) < 1.4e-5
+            @test abs(coff_shanks - _c1) < 7e-4
+        end
+    end
 end
